832a124e6005e3f8c1f6a4f7cd524c0036a41c45
大塚健登 <kent0tsuka1118@gmail.com>
2022-04-18 17:43:27 +0900

    Remove：USERテーブルのLINEBotともメッセージのやりとりを表すステータスカラムを削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,4 @@ class User < ApplicationRecord
   has_many :letters, dependent: :destroy
   has_many :records, dependent: :destroy
   has_many :comments, dependent: :destroy
-
-  enum letter_status: { send_letter: 0, letter_registration: 1, dig_registration: 2 }
-  enum record_status: { send_record: 0, select_category: 1, title_registration: 2, content_registration: 3 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_17_054739) do
+ActiveRecord::Schema.define(version: 2022_04_10_072446) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,8 +49,6 @@ ActiveRecord::Schema.define(version: 2022_04_17_054739) do
     t.string "line_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "letter_status", default: 0
-    t.integer "record_status", default: 0
   end
 
   add_foreign_key "comments", "records"
