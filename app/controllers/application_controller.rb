dfd3d582e432aa0e207136c7c0aab92f1f2d5e2b
大塚健登 <kent0tsuka1118@gmail.com>
2022-04-18 19:13:02 +0900

    Add：current_userメソッドを作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user
 
   private
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
 
   def set_liff_top_id
     gon.liff_id = ENV['LIFF_TOP_ID']
