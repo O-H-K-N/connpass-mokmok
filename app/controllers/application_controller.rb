26b9ab4ef58ac169d9e6d5dd7d6571c533652b6b
大塚健登 <kent0tsuka1118@gmail.com>
2022-04-19 01:22:38 +0900

    Add：未ログインの場合、トップページに遷移させる処理を実装 #10

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,15 @@
 class ApplicationController < ActionController::Base
   helper_method :current_user
+  before_action :login_required
 
   private
 
+  # 未ログインの場合、トップページに遷移する
+  def login_required
+    redirect_to root_path unless current_user
+  end
+
+  # ログイン中のユーザをcurrent_userとして扱える
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,3 +1,4 @@
 class HomesController < ApplicationController
+  skip_before_action :login_required
   def top;end
 end
