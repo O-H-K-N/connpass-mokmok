7b89fac682a5464040d4dffe5c9c3f6a617e895d
大塚健登 <kent0tsuka1118@gmail.com>
2022-04-23 00:13:28 +0900

    Add：記録一覧から詳細への遷移を実装・ヘッダーに切り替えメニューを設け、一覧と作成を見やすく実装 #23

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,15 @@
 module ApplicationHelper
+  # 記録を扱うページのヘッダーメニュー
+  def record_menu(target_action)
+    return unless params[:action] == target_action
+
+    'text-dark bg-light border border-3 rounded-2'
+  end
+
+  # 新規登録・ログイン後のフッターメニュー
   def footer_menu(target_controller)
     return unless params[:controller] == target_controller
 
-    'text-dark bg-light'
+    'text-dark bg-light border border-3 rounded-2'
   end
 end
