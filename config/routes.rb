25172320d6385b22a76158a8821b13ae1f60c2d1
大塚健登 <kent0tsuka1118@gmail.com>
2022-04-27 15:06:28 +0900

    Remove：リッチメニューからの遷移で不要な部分を削除 #15 #16

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,6 @@ Rails.application.routes.draw do
   root 'homes#top'
   get 'top', to: 'homes#top'
   get 'login', to: 'users#login'
-  get 'records/login', to: 'records#login'
-  post 'records/logedin', to: 'records#logedin'
-  get 'letters/login', to: 'letters#login'
-  post 'letters/logedin', to: 'letters#logedin'
   post '/callback' => 'linebot#callback'
   resource :user, only: %i[show create]
   resources :records, only: %i[index show new create destroy] do
