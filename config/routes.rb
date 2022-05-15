9cf3a4db85d5c9305f7ad9c43fb06c1e82c4bce8
大塚健登 <kent0tsuka1118@gmail.com>
2022-05-15 23:06:19 +0900

    Add：アプリの使い方ページを実装 #46

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'homes#top'
   get 'top', to: 'homes#top'
+  get 'use', to: 'homes#use'
   get 'privasy', to: 'homes#privasy'
   get 'terms', to: 'homes#terms'
   get 'contact', to: 'homes#contact'
