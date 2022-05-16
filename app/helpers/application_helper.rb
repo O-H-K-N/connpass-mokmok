72950353a549e804257a11d9d99ecb56bc79d9e7
大塚健登 <kent0tsuka1118@gmail.com>
2022-05-16 12:38:25 +0900

    Add：content_forを使ったタブタイトルの設定 #46

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
-  # 手紙を扱うページのヘッダーメニュー
-  def letter_menu(target_action)
-    return unless params[:action] == target_action
+  # タブタイトル
+  def page_title(page_title = '')
+    base_title = t('defaults.app_name')
 
-    'text-dark bg-light border border-3 rounded-2'
+		page_title.empty? ? base_title : page_title + " | " + base_title
   end
 
   # 新規登録・ログイン後のフッターメニュー
