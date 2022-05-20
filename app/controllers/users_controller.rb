26f22ee096d84b7db6e7a85f1dc77a1ed3f64b8a
大塚健登 <kent0tsuka1118@gmail.com>
2022-05-20 13:44:39 +0900

    Fix：キーワードに関するもくもく会一覧からのログイン処理を再度修正 #9

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
   # 「キーワードに関するもくもく会一覧」からのログイン
   def set; end
 
-  def index
+  def events
     # ユーザが登録したキーワードに関するもくもく会を取得
     url = URI.encode"https://connpass.com/api/v1/event/?keyword=もくもく会&keyword_or=#{current_user.word_first}&keyword_or=#{current_user.word_second}&keyword_or=#{current_user.word_third}&count=100&order=2"
     events = User.get_events(url)
